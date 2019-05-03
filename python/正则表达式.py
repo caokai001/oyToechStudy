43fc2e83e1d657404f75f5c8345590ecde4eb8ee
kcao <33218753+caokai001@users.noreply.github.com>
2019-05-03 23:52:30 +0800

    Update 正则表达式.py

--- python/正则表达式.py
+++ python/正则表达式.py
@@ -1,3 +1,4 @@
+###正则表达式1
 ##资料来源：https://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000/00143193331387014ccd1040c814dee8b2164bb4f064cff000
 1.re模块
 有了准备知识，我们就可以在Python中使用正则表达式了。Python提供re模块，包含所有正则表达式的功能。由于Python的字符串本身也用"\"转义，所以要特别注意：
@@ -58,5 +59,67 @@ def is_valid_email(addr):
      if re.match(r"^(.*?)[@#](\w+?).com$",addr): 
          print("ok") 
 is_valid_email('someone@gmail.com')
+
+
+
+
+
+###正则表达式2
+re 模块的一般使用步骤如下：
+    .使用 compile 函数将正则表达式的字符串形式编译为一个 Pattern 对象
+    .通过 Pattern 对象提供的一系列方法对文本进行匹配查找，获得匹配结果（一个 Match 对象）
+    .最后使用 Match 对象提供的属性和方法获得信息，根据需要进行其他的操作
+函数：
+    compile 函数：用于编译正则表达式，生成一个 Pattern 对象。
+                re.compile(pattern[, flag])
+            eg:  pattern = re.compile(r'\d+')
+    match 函数
+            用于查找字符串的头部（也可以指定起始位置），它是一次匹配，只要找到了一个匹配的结果就返回，而不是查找所有匹配的结果。
+                match(string[, pos[, endpos]])
+            eg： m = pattern.match('one12twothree34four')  ###None
+
+    search 函数
+            它也是一次匹配，只要找到了一个匹配的结果就返回.
+                search(string[, pos[, endpos]])
+            eg:  m = pattern.search('one12twothree34four')
+
+    findall 函数
+            搜索整个字符串，获得所有匹配的结果.返回列表
+                findall(string[, pos[, endpos]])
+            eg：  result2 = pattern.findall('one1two2three3four4', 0, 10)
+    finditer 函数
+            搜索整个字符串，获得所有匹配的结果.返回迭代器
+            
+            eg:     result_iter2 = pattern.finditer('one1two2three3four4', 0, 10)
+        
+        
+    split 函数
+            按照能够匹配的子串将字符串分割后.返回列表
+            split(string[, maxsplit])
+            eg:     p = re.compile(r'[\s\,\;]+')
+                    print (p.split('a,b;; c   d'))
+
+    sub 函数
+            sub 方法用于替换，返回是字符串。
+        import re
+        p = re.compile(r'(\w+) (\w+)')
+        s = 'hello 123, hello 456'
+        def func(m):
+            return 'hi' + ' ' + m.group(2)
+        print p.sub(r'hello world', s)  # 使用 'hello world' 替换 'hello 123' 和 'hello 456'
+        print p.sub(r'\2 \1', s)        # 引用分组
+        print p.sub(func, s)
+        print p.sub(func, s, 1) 
+        
+        ##结果：
+        hello world, hello world
+        123 hello, 456 hello
+        hi 123, hi 456
+        hi 123, hello 456
+        
+    subn 函数
+        和sub类似，但是返回的结果是元组：(sub(),替换次数)
+
+
 
 
